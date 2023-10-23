8997b309f58fa9197aabbdc2559835ae127140c0
Mereki <143884406+Mereki@users.noreply.github.com>
2023-10-22 19:43:49 -0500

    Update ascii_clock.py

--- ascii_clock.py
+++ ascii_clock.py
@@ -1,3 +1,41 @@
+#
+# time = input("Enter the time: ")
+# # clockType = input("Choose the clock type (12 or 24): ")
+# prefChar = input("Enter your preferred character: ")
+# checker = False
+# while not checker:
+#     invalidChars = ['abcdefghijklmnopqrstuvwxyz']
+#     if prefChar in invalidChars:
+#         prefChar = input("Character not permitted! Try again: ")
+#     else:
+#         checker = True
+#
+#
+# periodCheck = False
+#
+# sep = time.split(":")
+#
+# print(sep)
+#
+# # if clockType == "12":
+# #     periodCheck = True
+#
+# for i in time:
+#     print(i)
+
+# just in case lol
+# digit = [[['0', '0', '0'], ['0', ' ', '0'], ['0', ' ', '0'], ['0', ' ', '0'], ['0', '0', '0']],  # 0
+#          [[' ', '1', ' '], ['1', '1', ' '], [' ', '1', ' '], [' ', '1', ' '], ['1', '1', '1']],  # 1
+#          [['2', '2', '2'], [' ', ' ', '2'], ['2', '2', '2'], ['2', ' ', ' '], ['2', '2', '2']],  # 2
+#          [['*', '*', '*'], [' ', ' ', '*'], ['*', '*', '*'], [' ', ' ', '*'], ['*', '*', '*']],  # 3
+#          [['*', ' ', '*'], ['*', ' ', '*'], ['*', '*', '*'], [' ', ' ', '*'], [' ', ' ', '*']],  # 4
+#          [['*', '*', '*'], ['*', ' ', ' '], ['*', '*', '*'], [' ', ' ', '*'], ['*', '*', '*']],  # 5
+#          [['*', '*', '*'], ['*', ' ', ' '], ['*', '*', '*'], ['*', ' ', '*'], ['*', '*', '*']],  # 6
+#          [['*', '*', '*'], [' ', ' ', '*'], [' ', ' ', '*'], [' ', ' ', '*'], [' ', ' ', '*']],  # 7
+#          [['*', '*', '*'], ['*', ' ', '*'], ['*', '*', '*'], ['*', ' ', '*'], ['*', '*', '*']],  # 8
+#          [['*', '*', '*'], ['*', ' ', '*'], ['*', '*', '*'], [' ', ' ', '*'], ['*', '*', '*']],  # 9
+#          [[' '], ['*'], [' '], ['*'], [' ']]]  # :
+
 digit = [[['0', '0', '0'], ['0', ' ', '0'], ['0', ' ', '0'], ['0', ' ', '0'], ['0', '0', '0']],  # 0
          [[' ', '1', ' '], ['1', '1', ' '], [' ', '1', ' '], [' ', '1', ' '], ['1', '1', '1']],  # 1
          [['2', '2', '2'], [' ', ' ', '2'], ['2', '2', '2'], ['2', ' ', ' '], ['2', '2', '2']],  # 2
@@ -16,14 +54,14 @@ period = [[[' ', 'A', ' '], ['A', ' ', 'A'], ['A', 'A', 'A'], ['A', ' ', 'A'], [
            ['M', ' ', ' ', ' ', 'M']]]  # M
 
 time = input("Enter the time: ")
-clock_type = input("Choose the clock type: ")
+clock_type = input("Choose the clock type (12 or 24): ")
 pref_char = input("Enter your preferred character: ")
 
 # check if user input preferred character is a valid character
 char_checker = False
-bad_char = "abcdefghijklmnopqrstuvwxyz"
+good_char = "abcdeghkmnopqrsuvwxyz@$&*="
 while not char_checker:
-    if pref_char in bad_char:
+    if pref_char not in good_char:
         pref_char = input("Character not permitted! Try again: ")
     else:
         char_checker = True
@@ -32,6 +70,21 @@ while not char_checker:
 period_flag = False
 if clock_type == '12':
     period_flag = True
+
+# assuming default is AM, if original format is 24hr but user wants to convert, takes into consideration
+# we need to convert the two-digit numbers that are >= 13
+which_pd = False  # default will be AM
+if clock_type == '12':
+    if time[2] == ':':  # ex: 13:52 is a 24hr, and the colon is in index 2
+        time_temp = time
+        print(time_temp)
+        if int(time[0] + time[1]) > 12 and period_flag:
+            which_pd = True 
+            time = time_temp + "PM"
+        else:
+            time = time_temp + "AM"
+    else:
+        time = time + "AM"
 
 # convert 24hr to 12hr if 24hr >= 13:00
 first_two = time[0] + time[1]
@@ -48,18 +101,49 @@ if clock_type == '12' and time[1] != ':':
             time_temp = time
             time = first_digit + second_digit + time_temp[2:]
 
-# assuming default is AM, if original format is 24hr but user wants to convert, takes into consideration
-which_pd = False  # default will be AM
-if time[1] != ':':
-    if int(first_two) > 12 and period_flag:
-        which_pd = True
+if time[0] == '0' and clock_type == '12':
+    time = time.replace('0', '1')
 
-# what is this
-# ln1 = ""
-# ln2 = ""
-# ln3 = ""
-# ln4 = ""
-# ln5 = ""
+for i in range(5):
+    digit_line = ""
 
-# for i in range(5):
-#     if period_flag:
+    for index in range(len(time)):
+        char = time[index]
+        if period_flag:  # 12 hour time
+            if which_pd:  # if false (AM)
+                if char == ":":
+                    for j in digit[10][i]:
+                        digit_line += j
+                elif char == "A":
+                    for j in period[0][i]:
+                        digit_line += j
+                elif char == "P":
+                    for j in period[1][i]:
+                        digit_line += j
+                elif char == "M":
+                    for j in period[2][i]:
+                        digit_line += j
+                else:
+                    for j in digit[int(char)][i]:
+                        if j != ' ':  # then we need to possibly replace character
+                            if pref_char == '':
+                                digit_line += j
+                            else:
+                                digit_line += pref_char
+                        else:
+                            digit_line += ' '
+                digit_line += " "
+        else:
+            for j in digit[int(char)][i]:
+                if j != ' ':  # then we need to possibly replace character
+                    if pref_char == '':
+                        digit_line += j
+                    else:
+                        digit_line += pref_char
+                else:
+                    digit_line += ' '
+            digit_line += " "
+            # 24 hour time
+        if index == len(time) - 1:  # remove the last space
+            digit_line = digit_line[:-1]
+    print(digit_line)
